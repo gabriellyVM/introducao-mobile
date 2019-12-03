2f1c4aa747bb13333fea18d551e3b45a27f808fb
Your Name <you@example.com>
2019-12-03 15:50:48 -0300

    primeira versão projeto pro-chefe

---
+++ IntroducaoMobile/IntroducaoMobile/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IntroducaoMobile
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void logar(object sender, EventArgs e)
+        {
+            if (login.Text.Equals("senai") && senha.Text.Equals("123"))
+            {
+                Navigation.PushAsync(new Dashboard());
+            }
+            else
+            {
+                DisplayAlert("Alerta!", "Login Inválido", "Fechar");
+            }
+        }
+    }
+}
